049569876ab920d02dbdc63d3029927f0f45ea64
Anton Malkov <malferety@gmail.com>
2023-06-28 22:32:00 +0500

    Npm init, made function of distance and bought of bread

---
+++ app/test.js
@@ -0,0 +1,34 @@
+// var nastyaSpeed = 15;
+// var antonSpeed = 14;
+// var time = 3;
+// var nastyaDistance = nastyaSpeed * time;
+// var antonDistance = antonSpeed * time;
+// var totalDistance = antonDistance + nastyaDistance;
+
+var calculateDistance = function(time, nastyaSpeed, antonSpeed){
+  
+var nastyaDistance = nastyaSpeed * time;
+var antonDistance = antonSpeed * time;
+var totalDistance = antonDistance + nastyaDistance;
+  
+return totalDistance;
+};
+//console.log(calculateDistance(3, 12, 15)*3);
+
+var firstDistance = calculateDistance(3,12,14)*3;
+var secondDistance = calculateDistance(2,5,13);
+
+console.log(firstDistance, secondDistance);
+  
+//document.write(calculatelDistance);
+
+var buyBread = function(eggs){
+    if (eggs){
+        return 10;
+    }
+    else{
+        return 1;
+    }
+    //Можно без else, просто return
+};
+console.log(buyBread(true));
