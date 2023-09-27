075d3f4ae19825278929904e9a97308f590f80d0
Anton Malkov <malferety@gmail.com>
2023-09-27 21:50:45 +0500

    Read about JSON

---
+++ app/test3.js
@@ -0,0 +1,28 @@
+"use strick";
+// let user = {
+//     name: "John",
+//     age: 25,
+//     roles: {
+//       isAdmin: false,
+//       isEditor: true
+//     }
+//   };
+  
+//   alert(JSON.stringify(user, null, "А"));
+
+
+
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+let meetup = JSON.parse(str, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+
+alert( meetup.date.getDate() ); // 30 
+
+// for (key in Object.values(meetup)){
+//     alert(key);
+// }
+
+alert(Object.entries(meetup));
