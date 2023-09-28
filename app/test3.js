39762571f1887c736bcb1724d42bc7d9f3f3022a
YouAreNotHere <malferety@gmail.com>
2023-09-28 20:35:48 +0500

    Complit JSON task, start read about recursion

--- app/test3.js
+++ app/test3.js
@@ -12,17 +12,66 @@
 
 
 
-let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+// let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
 
-let meetup = JSON.parse(str, function(key, value) {
-  if (key == 'date') return new Date(value);
-  return value;
-});
+// let meetup = JSON.parse(str, function(key, value) {
+//   if (key == 'date') return new Date(value);
+//   return value;
+// });
 
-alert( meetup.date.getDate() ); // 30 
+// alert( meetup.date.getDate() ); // 30 
 
-// for (key in Object.values(meetup)){
-//     alert(key);
-// }
+// // for (key in Object.values(meetup)){
+// //     alert(key);
+// // }
 
-alert(Object.entries(meetup));
+// alert(Object.entries(meetup));
+
+
+// let user = {
+//   name: "Василий Иванович",
+//   age: 35
+// };
+
+// let str = JSON.stringify(user);
+// let user2 = JSON.parse(str);
+// alert(typeof(user2));
+// alert(user2.name);
+
+
+// let room = {
+//   number: 23
+// };
+
+// let meetup = {
+//   title: "Совещание",
+//   occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+//   place: room
+// };
+
+// // цикличные ссылки
+// room.occupiedBy = meetup;
+// meetup.self = meetup;
+
+// // const huy = JSON.stringify(meetup, function replacer(key, value){
+// //   return (value == "meetup") ? undefined : value;
+// // });
+
+// // alert(huy);
+
+// alert( JSON.stringify(meetup, function replacer(key, value) {
+//   return (key != "" && value == meetup) ? undefined : value;
+// }));
+
+// alert( JSON.stringify(meetup, function replacer(key, value) {
+//   return (key != "" && value == meetup) ? undefined : value;
+// }));
+
+
+function sum(a,b){
+  const c = a + b;
+  sum(a,b);
+  return c;
+}
+
+alert(sum(2,3));
