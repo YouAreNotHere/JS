283130630f6a4bc93dad111b9323e402e9dc9eb1
Anton Malkov <malferety@gmail.com>
2023-12-06 21:13:36 +0500

    Still learn classes

--- app/test3.js
+++ app/test3.js
@@ -789,30 +789,82 @@
 
 
 
-let animal = {
-  sayHi() {
-    alert("Я животное");
+// let animal = {
+//   sayHi() {
+//     alert("Я животное");
+//   }
+// };
+
+// // rabbit наследует от animal
+// let rabbit = {
+//   __proto__: animal,
+//   sayHi() {
+//     super.sayHi();
+//   }
+// };
+
+// let plant = {
+//   sayHi() {
+//     alert("Я растение");
+//   }
+// };
+
+// // tree наследует от plant
+// let tree = {
+//   __proto__: plant,
+//  // sayHi: rabbit.sayHi // (*)
+// };
+
+// tree.sayHi();  // Я животное (?!?)
+
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+class ExtendedClock extends Clock {
+  constructor(options) {
+    super(options);
+    let { precision = 1000 } = options;
+    this.precision = precision;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
   }
 };
 
-// rabbit наследует от animal
-let rabbit = {
-  __proto__: animal,
-  sayHi() {
-    super.sayHi();
-  }
-};
-
-let plant = {
-  sayHi() {
-    alert("Я растение");
-  }
-};
-
-// tree наследует от plant
-let tree = {
-  __proto__: plant,
- // sayHi: rabbit.sayHi // (*)
-};
-
-tree.sayHi();  // Я животное (?!?)
+ let newClock = new ExtendedClock({template: 'h:m:s'},3000);
+newClock.start();
